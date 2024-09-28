7fedcdf8f212891a0217e2f6813bfba6cf570768
michalgs <michalgsmoragiewicz@gmail.com>
2024-09-28 17:58:34 +0200

    Added Route and UserReport classes

---
+++ lib/dataclasses/route.dart
@@ -0,0 +1,9 @@
+class Route {
+  late String startLocation;
+  late String destinationLocation;
+  late double routeLength;
+  late String gpx;
+  late double safetyRating;
+  late double userRating;
+  
+}
